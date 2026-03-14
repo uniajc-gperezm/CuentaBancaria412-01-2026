831b082143eee9154e04c2a0820f49e25547099b
JeanRojas17 <jeanpaulrojas17@gmail.com>
2026-03-13 20:51:36 -0500

    Modificando todas las clases (Ejercicio resuelto)

--- src/main/java/com/uniajc/CuentaAhorro.java
+++ src/main/java/com/uniajc/CuentaAhorro.java
@@ -4,23 +4,43 @@ public class CuentaAhorro extends Cuenta {
 
     protected boolean activa;
 
-    public CuentaAhorro(float saldo, float tasaAnual) {
-        super(saldo, tasaAnual);
+    public CuentaAhorro(float saldo, float tasa) {
+        super(saldo, tasa);
+        activa = saldo >= 10000;
     }
 
+    @Override
     public void consignar(float cantidad) {
-        
+        if(activa) {
+            super.consignar(cantidad);
+        } else {
+            System.out.println("Cuenta inactiva. No se pueden realizar consignaciones.");
+        }
     }
 
+    @Override
     public void retirar(float cantidad) {
-        
+        if(activa) {
+            super.retirar(cantidad);
+        } else {
+            System.out.println("Cuenta inactiva. No se pueden realizar retiros.");
+        }
     }
 
     public void extractoMensual() {
-        
+        if(numeroRetiros > 4) {
+            comisionMensual = (numeroRetiros - 4) * 1000;
+        }
+
+        super.extractoMensual();
+        activa = saldo >= 10000;
     }
-    
+
     public void imprimir() {
-        
+        System.out.println("\n* * * *   Cuenta de Ahorros   * * * *");
+        System.out.println("Saldo: $" +saldo);
+        System.out.println("Comision mensual: $" +comisionMensual);
+        System.out.println("Número de transacciones: " +(numeroConsignaciones + numeroRetiros));
+        System.out.println("Cuenta activa: " +(activa ? "Sí" : "No"));
     }
 }
--- src/main/java/com/uniajc/Main.java
+++ src/main/java/com/uniajc/Main.java
@@ -2,4 +2,17 @@ package com.uniajc;
 
 public class Main {
 
+    public static void main(String[] args) {
+        CuentaAhorro ca = new CuentaAhorro(50000, 12);
+
+        ca.consignar(20000);
+        ca.consignar(10000);
+        ca.retirar(15000);
+        ca.retirar(23000);
+        ca.retirar(22000);
+        ca.retirar(7000);
+        ca.retirar(1000);
+        ca.extractoMensual();
+        ca.imprimir();
+    }
 }
