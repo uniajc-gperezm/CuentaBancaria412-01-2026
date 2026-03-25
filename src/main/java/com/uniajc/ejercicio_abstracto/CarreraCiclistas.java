0022359d26e9055ec3d617f8f5bbafc247b41749
Jennifer Villamillan <jennifervillamillan11@gmail.com>
2026-03-24 20:05:03 -1200

    se adicionan  equipos

--- src/main/java/com/uniajc/ejercicio_abstracto/CarreraCiclistas.java
+++ src/main/java/com/uniajc/ejercicio_abstracto/CarreraCiclistas.java
@@ -2,7 +2,13 @@ package com.uniajc.ejercicio_abstracto;
 
 public class CarreraCiclistas {
     public static void main(String[] args) {
+
+        // se crean los equipos 
+        
         Equipo equipo = new Equipo("Rayo Azul", "Colombia");
+        Equipo equipo2 = new Equipo("Purple ", "España");
+        Equipo equipo3 = new Equipo("Amercica ", "Colombia");
+        Equipo equipo4 = new Equipo("cali", "Colombia");
 
         Velocista v1 = new Velocista(1, "Camila", 410.5, 65.2);
         v1.setTiempoAcumulado(120);
@@ -12,6 +18,11 @@ public class CarreraCiclistas {
 
         Contrarrelojista c1 = new Contrarrelojista(3, "Sofia", 72.0);
         c1.setTiempoAcumulado(128);
+        
+          
+          
+
+
 
         equipo.anadirCiclista(v1);
         equipo.anadirCiclista(e1);
