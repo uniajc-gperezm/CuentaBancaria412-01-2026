3c34ae1d54f49268fa7a1cac4965e540313702dc
Carlos Alberto Obando <calbertoobando@estudiante.uniajc.edu.co>
2026-03-24 21:21:06 -0500

    Se agregan cambios solicitados por el profesor

--- src/main/java/com/uniajc/ejercicio_abstracto/CarreraCiclista.java
+++ src/main/java/com/uniajc/ejercicio_abstracto/CarreraCiclista.java
@@ -1,9 +1,11 @@
-ublic class Main {
-    public static void main(String[] args) {
-        Equipo equipo = new Equipo("");
+package com.uniajc.ejercicio_abstracto;
 
-        Velocista v = new Velocista();
-        Escalador e = new Escalador();
+public class CarreraCiclista {
+    public static void main(String[] args) {
+        Equipo equipo = new Equipo("Equipo A", "Colombia");
+
+        Velocista v = new Velocista(2, "Carlos", 1, 1);
+        Escalador e = new Escalador(1, "Alberto", 20, 15);
         Contrarrelojista c = new Contrarrelojista(3, "Ana", 60);
 
         equipo.añadirCiclista(v);
@@ -14,6 +16,6 @@ ublic class Main {
         equipo.listarNombresCiclistas();
 
         System.out.println("\nDatos de un ciclista:");
-        equipo.imprimirDatosCiclistaPorId();
+        equipo.imprimirDatosCiclistaPorId(1);
     }
 }
