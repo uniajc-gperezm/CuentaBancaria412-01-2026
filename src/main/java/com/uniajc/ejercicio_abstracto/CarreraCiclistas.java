01d0a4e6bbc4d872d954d7781f2a6c08bee87257
Michael Lenis <lenismaicol9@gmail.com>
2026-03-24 19:56:10 -0500

    Ejecutador terminado

--- src/main/java/com/uniajc/ejercicio_abstracto/CarreraCiclistas.java
+++ src/main/java/com/uniajc/ejercicio_abstracto/CarreraCiclistas.java
@@ -1,7 +1,31 @@
 package com.uniajc.ejercicio_abstracto;
 
 public class CarreraCiclistas {
-    public static void main(String[] args) {
-       
+   public static void main(String[] args) {
+
+        Equipo equipo = new Equipo("Movistar", "España");
+
+        Velocista v = new Velocista(1, "Juan", 400, 60);
+        Escalador e = new Escalador(2, "Carlos", 2.5f, 15);
+        Contrarrelojista c = new Contrarrelojista(3, "Luis", 70);
+
+        v.setTiempoAcumulado(120);
+        e.setTiempoAcumulado(150);
+        c.setTiempoAcumulado(110);
+
+        equipo.añadirCiclista(v);
+        equipo.añadirCiclista(e);
+        equipo.añadirCiclista(c);
+
+        equipo.imprimirDatosEquipo();
+
+        System.out.println("Ciclistas:");
+        equipo.listarCiclistas();
+
+        equipo.calcularTiempoTotal();
+        System.out.println("Tiempo total equipo: " + Equipo.getTiempoTotalEquipo());
+
+        System.out.println("Buscar ciclista ID 2:");
+        equipo.buscarCiclista(2);
     }
 }
