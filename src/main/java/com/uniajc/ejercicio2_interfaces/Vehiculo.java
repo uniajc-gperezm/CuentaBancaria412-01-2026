906b1cbdb8b564fca37419fb760e9bf8ee31f5b7
JeanRojas17 <jeanpaulrojas17@gmail.com>
2026-03-25 20:45:32 -0500

    Añadiendo clase abstracta Vehiculo

---
+++ src/main/java/com/uniajc/ejercicio2_interfaces/Vehiculo.java
@@ -0,0 +1,21 @@
+package com.uniajc.ejercicio2_interfaces;
+
+public abstract class Vehiculo {
+    
+    protected int velocidadActual;
+    protected int velocidadMaxima;
+
+    public Vehiculo(int velocidadActual, int velocidadMaxima) {
+        this.velocidadActual = velocidadActual;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void imprimir() {
+        System.out.println("Velocidad Actual: " +velocidadActual+ " km/h");
+        System.out.println("Velocidad Máxima: " +velocidadMaxima+ " km/h");
+    }
+
+    public abstract void acelerar(int velocidad);
+
+    public abstract void frenar(int velocidad);
+}
