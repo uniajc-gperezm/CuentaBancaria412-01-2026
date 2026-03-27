c68cc299f95396d27587b0d60a3e9fcd80d68b2a
CristianErazo-hub <cristianerazo420@gmail.com>
2026-03-27 16:02:18 -0500

    Creando clase Ballena que extiende a la clase Mamifero

---
+++ src/main/java/com/uniajc/ejercicio1_interfaces/Ballena.java
@@ -0,0 +1,11 @@
+package com.uniajc.ejercicio1_interfaces;
+
+public class Ballena extends Mamifero {
+
+    @Override
+    Public void amamantarCrias() {
+
+        System.out.println("La ballena puede amamantar a sus crías.");
+    }
+
+}
