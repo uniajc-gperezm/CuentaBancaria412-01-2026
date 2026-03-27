f116853265822aabb30e4125672742088365f294
CristianErazo-hub <cristianerazo420@gmail.com>
2026-03-27 17:37:32 -0500

    Agregando objetos

--- src/main/java/com/uniajc/ejercicio2_interfaces/Main.java
+++ src/main/java/com/uniajc/ejercicio2_interfaces/Main.java
@@ -3,7 +3,20 @@ package com.uniajc.ejercicio2_interfaces;
 public class Main {
 
     public static void main(String[] args) {
-        
-    }
 
+        Terrestre carro = new Terrestre(50, 20, 4, "Ciudadano");
+
+        carro.acelerar(30);
+        carro.frenar(20);
+        carro.imprimir();
+
+        int revoluciones = carro.calcularRevolucionesMotor(10, 5);
+        System.out.println("Las revoluciones del motor son: " + revoluciones);
+
+        Acuatico lancha = new Acuatico(50, 110, "Mar mediano", 2);
+        lancha.acelerar(40);
+        lancha.recomendarVelocidad(85);
+        lancha.imprimir();
+
+    }
 }
