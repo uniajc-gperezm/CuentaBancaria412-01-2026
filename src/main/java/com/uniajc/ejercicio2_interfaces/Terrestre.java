19b6e5bb7e6c0355e14b6dd63702820c13a4d7ec
CristianErazo-hub <cristianerazo420@gmail.com>
2026-03-27 16:57:24 -0500

    Creando herencia Terrestre

---
+++ src/main/java/com/uniajc/ejercicio2_interfaces/Terrestre.java
@@ -0,0 +1,43 @@
+package com.uniajc.ejercicio2_interfaces;
+
+public class Terrestre extends Vehiculo implements Motor {
+
+    private int cantidadDeLlantas;
+    private String uso;
+
+    public Terrestre(int velocidadActual, int velocidadMaxima, int llantas, String uso) {
+
+        super(velocidadActual, velocidadMaxima);
+        this.cantidadDeLlantas = llantas;
+        this.uso = uso;
+
+    }
+
+    @Override
+    
+    public void acelerar(int velocidad) {
+
+        velocidadActual += velocidad;
+
+        if (velocidadActual > velocidadMaxima) {
+            velocidadActual = velocidadMaxima;
+        }
+    }
+
+    @Override
+
+    public int calcularRevolucionesMotor(int fuerza, int radio) {
+
+        return (fuerza * radio);
+    }
+
+    @Override
+
+    public void imprimir() {
+
+        super.imprimir();
+
+        System.out.println("Cantidad de llantas; " + cantidadDeLlantas);
+        System.out.println("USo: " + uso);
+    }
+}
