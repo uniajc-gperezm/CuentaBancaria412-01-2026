aff5bc684e432afd2ef66b6dbf4a8a89c94e6805
OnlyWhoo <jdavorgor@gmail.com>
2026-03-27 17:26:38 -0500

    Crear clase ornitorrinco

---
+++ src/main/java/com/uniajc/ejercicio_interfaz/Ornitorrinco.java
@@ -0,0 +1,15 @@
+package com.uniajc.ejercicio_interfaz;
+
+public class Ornitorrinco extends Mamifero implements Oviparo {
+
+    @Override
+    public void amamantarCrias() {
+        System.out.println("El ornitorrinco amamanta a sus crías.");
+    }
+
+    @Override
+    public void ponerHuevos() {
+        System.out.println("El ornitorrinco pone huevos.");
+    }
+
+}
