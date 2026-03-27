62663e2b8b75359b2409a166194bf9eb5a8b1e01
Gustavo <gustavo@example.com>
2026-03-27 18:57:30 -0500

    Agrega clase Ballena que extiende Mamifero

---
+++ src/main/java/com/uniajc/ejercicio1_interfaces/Ballena.java
@@ -0,0 +1,9 @@
+package com.uniajc.ejercicio1_interfaces;
+
+public class Ballena extends Mamifero {
+
+    @Override
+    public void amamentarCrias() {
+        System.out.println("La ballena amamanta a sus crías");
+    }
+}
