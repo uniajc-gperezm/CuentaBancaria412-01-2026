91bab0cbc059783410f9f5724d596efe004f4f4a
Gustavo <gustavo@example.com>
2026-03-27 19:05:00 -0500

    Implementa métodos main para demostrar ejercicios de interfaces

---
+++ src/main/java/com/uniajc/ejercicio1_interfaces/EjercicioUnoInterfaces.java
@@ -0,0 +1,12 @@
+package com.uniajc.ejercicio1_interfaces;
+
+public class EjercicioUnoInterfaces {
+    public static void main(String[] args) {
+        Ballena ballena = new Ballena();
+        ballena.amamentarCrias();
+
+        Ornitorrinco ornitorrinco = new Ornitorrinco();
+        ornitorrinco.amamentarCrias();
+        ornitorrinco.ponerHuevos();
+    }
+}
