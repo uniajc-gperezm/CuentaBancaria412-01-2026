f7fae458a5f93304555edfe758dd6d9196df49f5
Daniel sundar Bonilla <danielsundar01011234@gmail.com>
2026-04-01 01:29:07 +0200

    Implementacion de clase Ballena

---
+++ src/main/java/com/uniajc/ejercicio1_interfaces/Ballena.java
@@ -0,0 +1,11 @@
+package com.uniajc.ejercicio1_interfaces;
+
+public class Ballena extends Mamiferos {
+
+    @Override
+    public void amamantarCrias() {
+        String mensaje = "La ballena alimenta a sus crías con leche en el océano.";
+        System.out.println(mensaje);
+    }
+
+}
